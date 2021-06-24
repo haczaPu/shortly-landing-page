95304d814e4c0aedcf162c6998dac73ef947c6d4
wgrabowski <wgrabowski@man.poznan.pl>
2021-06-24 15:44:38 +0200

    few minor style bug fixed

--- src/components/LinksWrapper.js
+++ src/components/LinksWrapper.js
@@ -8,14 +8,14 @@ const LinksWrapper = () => {
           <div className="result__link">https://frontendmenor.io</div>
           <div className="result__container">
             <div className="result__link result__link--shorten">https://frfdssr.io</div>
-            <button className="btn btn--highlighted">Copy</button>
+            <button className="btn btn--highlighted btn--squared">Copy</button>
           </div>
         </div>
         <div className="result">
           <div className="result__link">https://frontendmenor.io</div>
           <div className="result__container">
             <div className="result__link result__link--shorten">https://frfdssr.io</div>
-            <button className="btn btn--highlighted">Copy</button>
+            <button className="btn btn--highlighted btn--squared">Copy</button>
           </div>
         </div>
       </div>
